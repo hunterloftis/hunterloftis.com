0385baaa8e65417a2251b970bddc5e7720566d35
Hunter Loftis <hunter.loftis+github@gmail.com>
2011-05-17 17:50:15 -0400

    Creating response paragraphs for each topic

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -1,7 +1,31 @@
 (function(_, $, ko) {
 
+  // Shorthand
+  
+  ko.obs = ko.observable;
+  ko.obsa = ko.observableArray;
+  ko.dobs = ko.dependentObservable;
+  
   function ViewModel() {
-    this.topics = [];
+    this.topics = ko.obsa([
+      { value: '', verb: 'do:', noun: '...' },
+      { value: 'js', verb: 'love', noun: 'JavaScript.' },
+      { value: 'apps', verb: 'build', noun: 'web apps.' },
+      { value: 'html5', verb: 'embrace', noun: 'HTML 5.' },
+      { value: 'mobile', verb: 'create', noun: 'the mobile web.' },
+      { value: 'ux', verb: 'obsess over', noun: 'user experience.' },
+      { value: 'design', verb: 'exhibit', noun: 'beautiful design.' },
+      { value: 'realtime', verb: 'develop', noun: 'realtime interactions.' }
+    ]);
+    this.activeTopic = ko.obs(this.topics()[0]);
+    this.activeVerb = ko.dobs(function() {
+      return this.activeTopic().verb;
+    }, this);
   }
+  
+  $(document).ready(function() {
+    var viewModel = new ViewModel();
+    ko.applyBindings(viewModel);
+  });
   
 })(_, $, ko);
